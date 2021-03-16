8936ed377b31fb26155ea72d9f8990dc9b59c026
thehaigo <sirannha@gmail.com>
2021-03-17 02:05:55 +0900

    presence liveview

--- lib/neos_socket_web/live/room_live/show.ex
+++ lib/neos_socket_web/live/room_live/show.ex
@@ -2,20 +2,31 @@ defmodule NeosSocketWeb.RoomLive.Show do
   use NeosSocketWeb, :live_view
 
   alias NeosSocket.Rooms
+  alias NeosSocketWeb.Presence
 
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, socket}
+  def mount(%{"id" => id}, _session, socket) do
+    obj_id = 9999
+    room = Rooms.get_room!(id)
+    NeosSocketWeb.Endpoint.subscribe("room:#{id}")
+    Presence.track_presence(
+      self(),
+      "room:#{id}",
+      obj_id,
+      %{ name: "liveview", id: obj_id}
+    )
+
+    {
+      :ok,
+      socket
+      |> assign(:page_title, "Room")
+      |> assign(:room, room)
+      |> assign(:object_id, obj_id)
+      |> assign(:users, Presence.list_presence("room:#{id}"))
+    }
   end
 
-  @impl true
-  def handle_params(%{"id" => id}, _, socket) do
-    {:noreply,
-     socket
-     |> assign(:page_title, page_title(socket.assigns.live_action))
-     |> assign(:room, Rooms.get_room!(id))}
-  end
-
-  defp page_title(:show), do: "Show Room"
-  defp page_title(:edit), do: "Edit Room"
+  def handle_info(%{event: "presence_diff"}, socket = %{assigns: %{room: room}}) do
+    {:noreply, assign(socket, users: Presence.list_presence("room:#{room.id}"))}
+  end  
 end
