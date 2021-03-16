f197e415c996bfff0c7ebe6a2cc51131e0732454
thehaigo <sirannha@gmail.com>
2021-03-17 00:14:02 +0900

    buluma add and styling

--- lib/neos_socket_web/live/modal_component.ex
+++ lib/neos_socket_web/live/modal_component.ex
@@ -11,9 +11,11 @@ defmodule NeosSocketWeb.ModalComponent do
       phx-target="#<%= @id %>"
       phx-page-loading>
 
-      <div class="phx-modal-content">
-        <%= live_patch raw("&times;"), to: @return_to, class: "phx-modal-close" %>
-        <%= live_component @socket, @component, @opts %>
+      <div class="phx-modal-content card">
+        <div>
+          <%= live_patch raw("&times;"), to: @return_to, class: "phx-modal-close" %>
+          <%= live_component @socket, @component, @opts %>
+        </div>
       </div>
     </div>
     """
