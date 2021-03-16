b2c81bd30a285671a453cd8d046812f34b45aed3
thehaigo <sirannha@gmail.com>
2021-03-16 23:11:47 +0900

    channel enable

--- lib/neos_socket_web/channels/user_socket.ex
+++ lib/neos_socket_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule NeosSocketWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", NeosSocketWeb.RoomChannel
+  channel "room:*", NeosSocketWeb.RoomChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
