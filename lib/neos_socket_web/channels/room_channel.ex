26b3e10f6d681338971619966d172aa551cbafad
thehaigo <sirannha@gmail.com>
2021-03-17 15:09:51 +0900

    create sender

--- lib/neos_socket_web/channels/room_channel.ex
+++ lib/neos_socket_web/channels/room_channel.ex
@@ -28,6 +28,27 @@ defmodule NeosSocketWeb.RoomChannel do
     end
   end
 
+  @impl true
+  def handle_in("broadcast", payload, socket) do
+    message = Rooms.create_message(payload).changes
+    broadcast!(socket, "broadcast_room", message)
+    {:reply, {:ok, message }, socket}
+  end
+
+  @impl
+  def handle_in("post", payload, socket) do
+    message = Rooms.create_message(payload).changes
+    presence = Presence.get_presence("room:#{message.room_id}", message.object_id)
+    Presence.update_presence(
+      self(),
+      "room:#{message.room_id}",
+      message.object_id,
+      %{messages: [message | presence.messages] |> List.flatten }
+    )
+
+    {:reply, :ok, socket}
+  end
+
   @impl
   def handle_info(%{event: "presence_diff"}, socket = %{ assigns: %{room_id: room_id } }) do
     {:noreply, assign(socket, users: Presence.list_presence("room:#{room_id}"))}
--- lib/neos_socket_web/live/room_live/show.ex
+++ lib/neos_socket_web/live/room_live/show.ex
@@ -2,12 +2,14 @@ defmodule NeosSocketWeb.RoomLive.Show do
   use NeosSocketWeb, :live_view
 
   alias NeosSocket.Rooms
+  alias NeosSocket.Rooms.Message
   alias NeosSocketWeb.Presence
 
   @impl true
   def mount(%{"id" => id}, _session, socket) do
     obj_id = 9999
     room = Rooms.get_room!(id)
+    changeset = Rooms.change_message(%Message{object_id: obj_id, room_id: id})
     NeosSocketWeb.Endpoint.subscribe("room:#{id}")
     Presence.track_presence(
       self(),
@@ -22,11 +24,30 @@ defmodule NeosSocketWeb.RoomLive.Show do
       |> assign(:page_title, "Room")
       |> assign(:room, room)
       |> assign(:object_id, obj_id)
+      |> assign(:changeset, changeset)
       |> assign(:users, Presence.list_presence("room:#{id}"))
     }
   end
 
+  @impl true
+  def handle_event("send", %{"message" => message_params}, socket) do
+    message = Rooms.create_message(message_params).changes
+    type = if message.object_id == 0, do: "room", else: "user"
+    topic = if message.object_id == 0 do
+              "#{type}:#{message.room_id}"
+            else
+              "#{type}:#{message.object_id}"
+            end
+
+    NeosWeb.Endpoint.broadcast!(
+      topic,
+      "broadcast_#{type}",
+      message
+    )
+    {:noreply, socket}
+  end
+
   def handle_info(%{event: "presence_diff"}, socket = %{assigns: %{room: room}}) do
     {:noreply, assign(socket, users: Presence.list_presence("room:#{room.id}"))}
-  end  
+  end
 end
