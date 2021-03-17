5943d298e281c63aec3584d70a000ecf9e20985d
thehaigo <sirannha@gmail.com>
2021-03-17 22:41:22 +0900

    channel test

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -9,5 +9,29 @@ channel.join()
   .receive("ok", resp => { console.log("joined successfully", resp)})
   .receive("error", resp => {console.log("unable to join", resp)})
 
+channel.on("broadcast_room", payload => {
+  console.log("receive_room_broadcast", payload)
+})
+
+channel.on("send_broadcast_user", payload => {
+  console.log("receive_user_broadcast", payload)
+})
+
+
+setTimeout(
+  function () {
+    channel.push("post", {body: "first", room_id: 1, object_id: 1})
+      .receive("ok", (resp) => console.log("first post ok:", resp))
+  },
+  "1000"
+);
+
+setTimeout(
+  function () {
+    channel.push("broadcast", {body: "broadcast", room_id: 1, object_id: 1})
+      .receive("ok", (resp) => console.log("broadcast send ok:", resp))
+  },
+  "1000"
+);
 
 export default socket
